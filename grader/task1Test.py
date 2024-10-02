366287a2d737f20d1a705f84fb7ae008f8325fbf
rcmurray <>
2024-10-02 13:17:52 -0400

    Trying to implement temp test code for Task 1

--- grader/task1Test.py
+++ grader/task1Test.py
@@ -1,7 +1,11 @@
 from test_utility import *
 import numpy as np
+from feedback import *
 
 task_id = 1
+
+positive_test_cases=['2 hours','4-5 hours','1 hour','2-3 hours','1 HOUR','24 hours']
+negative_test_cases=['Overnight','Hour','two']
 
 def solution(x):
 	return np.cumsum(x) / (1 + np.arange(len(x)))
@@ -11,26 +15,23 @@ def compare_answers(student_out, expected_out):
 	return all(np.asarray(student_out) == np.asarray(expected_out))
 
 
-def test_task(cumulative_avg):
-	x = np.arange(10).astype(float)
-	pass1, feedback1 = test_correctness_and_efficiency(
-		cumulative_avg, solution, compare_answers,
-		1, f"Input: x = {x}", 0.0, x
-	)
+def test_task(student_solution):
+    return "Code update is being used"
+	# return_feedback(student_solution,positive_test_cases,negative_test_cases,NAME)
 
-	np.random.seed(0)
-	x = np.random.randint(low = 0, high = 10, size = 1000000).astype(float)
-	pass2, feedback2 = test_correctness_and_efficiency(
-		cumulative_avg, solution, compare_answers,
-		2, f"Input: x = {x}", 10.0, x
-	)
+	# np.random.seed(0)
+	# x = np.random.randint(low = 0, high = 10, size = 1000000).astype(float)
+	# pass2, feedback2 = test_correctness_and_efficiency(
+	# 	cumulative_avg, solution, compare_answers,
+	# 	2, f"Input: x = {x}", 10.0, x
+	# )
 
-	if pass1 and pass2:
-		record_success(task_id)
-	else:
-		record_fail(task_id)
+	# if pass1 and pass2:
+	# 	record_success(task_id)
+	# else:
+	# 	record_fail(task_id)
 
-	record_end(task_id)
-	return pass1 and pass2, feedback1 + "\n" + feedback2
+	# record_end(task_id)
+	# return pass1 and pass2, feedback1 + "\n" + feedback2
 
 
